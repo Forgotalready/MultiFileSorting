ea52c7e0acb35b1a94a7bec9e60c92b2b67e35af
Forgotalready <113277258+Forgotalready@users.noreply.github.com>
2023-03-15 23:33:27 +0700

    Add files via upload

---
+++ MultiFileSotring/MultiFileSotring.h
@@ -0,0 +1,33 @@
+//
+// Created by Олег on 22.02.2023.
+//
+
+#ifndef MULTIFILESOTRING_MULTIFILESOTRING_H
+#define MULTIFILESOTRING_MULTIFILESOTRING_H
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <ctime>
+#include <vector>
+#include <utility>
+
+using std::cin, std::cout, std::fstream, std::string, std::vector;
+
+void divide(const string& name, vector <string>& out_names);
+
+vector<string> generate_file_names(int n);
+
+void create_file(const string& name);
+
+void gen_rand_file(const string& name, int n);
+
+std::pair<bool, int> isSortAndCount(const string& name);
+
+void name(vector<string> &name, int n);
+
+bool merge(vector<string> &names);
+
+int Min(vector<int>&x, int n);
+
+#endif //MULTIFILESOTRING_MULTIFILESOTRING_H
